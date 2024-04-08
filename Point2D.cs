2413cfda792a849d58a79c16849f6bc5e3727e45
trung6755 <trungggrr@gmail.com>
2024-04-08 16:31:24 +0700

    Initial commit

---
+++ Point2D.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace point
+{
+    public class Point2D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        // Constructor
+        public Point2D(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        // Method to calculate the distance between two points
+        public double CalculateDistance(Point2D otherPoint)
+        {
+            double dx = X - otherPoint.X;
+            double dy = Y - otherPoint.Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+        // Method to move the point by specified offsets
+        public void Move(double dx, double dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+
+        // Override ToString() method for better representation
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+
+    public class Point3D : Point2D
+    {
+        private float Z;
+
+        public Point3D(float x, float y, float z) : base(x, y)
+        {
+            this.Z = z;
+        }
+
+        public float GetZ()
+        {
+            return Z;
+        }
+
+        public void SetZ(float z)
+        {
+            this.Z = z;
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y}, {Z})";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point2D point2D = new Point2D(2, 5);
+            Console.WriteLine(point2D);
+
+            Point3D point3D = new Point3D(2, 5, 6);
+            Console.WriteLine(point3D);
+        }
+    }
+}
